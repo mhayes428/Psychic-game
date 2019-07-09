b433a5a0ed2f6fbc874a46a6b9dc615f306c0583
Mike Hayes <fikes428@gmail.com>
2019-07-09 14:15:46 -0400

    htmltextcorrection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,11 @@
-let computerChoices = ["a", "b", "c", "d", "e","f","g","h"]
+let computerChoices = ["a", "b", "c", "d"]
+let userChoices = ["a", "b", "c", "d"]
+
+let wins = 0;
+let losses = 0;
+let guesses = 10;
+let guessChoice = [];
+
+document.onkeyup = function (event) {
+    let userGuess = event.key;
+}
