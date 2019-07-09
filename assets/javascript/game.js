77a904dc81be5d910ddadab61e4bce14e92b3a07
Mike Hayes <fikes428@gmail.com>
2019-07-09 14:58:48 -0400

    jstuning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,58 @@
-let computerChoices = ["a", "b", "c", "d"]
-let userChoices = ["a", "b", "c", "d"]
+//Array
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+//Variables for Wins/Losses/Guesses
 let wins = 0;
 let losses = 0;
-let guesses = 10;
+let guessesLeft = 9;
 let guessChoice = [];
+let computerGuess = [];
 
-document.onkeyup = function (event) {
-    let userGuess = event.key;
+//Starts computer's guess
+window.onload = function() {
+    let compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+
+//Game Section
+document.onkeyup = function(event) {
+    let playerGuess = event.key;
+    lettersGuessed.push(playerGuess);
+    console.log(playerGuess[0]);
+}
+
+if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
+    wins++;
+    guessesLeft = 9;
+    lettersGuessed.length = 0;
+    computerGuess.length = 0;
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+    guessesLeft = guessesLeft - 1;
+}
+
+else {
+    losses++;
+    guessesLeft = 9;
+    lettersGuessed.length = 0;
+    computerGuess.length = 0;
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+var html = "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses left: " + guessesLeft + "</p>" +
+    "<p>Your guesses so far: " + lettersGuessed + "</p>";
+
+document.querySelector("#game").innerHTML = html;
+
 }
